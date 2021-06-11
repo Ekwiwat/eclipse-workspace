85b6db33d996db30fafcdd7895bff83efd60d50c
Ekwiwat <74827964+Ekwiwat@users.noreply.github.com>
2021-06-11 16:21:26 +0700

    Exercise 4.1 committed

---
+++ HelloWorld/src/com/ekwiwat/javaPart1Ex4_1/package-info.java
@@ -0,0 +1,34 @@
+package com.ekwiwat.javaPart1Ex4_1;
+
+class exercise4_1 {
+	public static void main(String[] args) {
+		
+//		ข้อ 4.1.1
+		int score = 49;
+		
+		if(score >= 80) {
+			System.out.println("Good");
+		} else if(score >= 50) {
+			System.out.println("Normal");
+			
+		} else {
+			System.out.println("Fail");
+		}
+		
+//		ข้อ 4.1.2
+		boolean isHandsome = true;
+		
+		if(isHandsome) {
+			System.out.println("Good");
+		} else if
+		(score >= 80) {
+			System.out.println("Good");
+		} else if(score >= 50) {
+			System.out.println("Normal");
+			
+		} else {
+			System.out.println("Fail");
+		}
+		
+	}
+}
