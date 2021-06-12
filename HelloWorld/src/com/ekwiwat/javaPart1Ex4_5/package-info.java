d4ae6140fda323fbc65afa124c845d42f90dfdbc
Ekwiwat <74827964+Ekwiwat@users.noreply.github.com>
2021-06-12 14:19:15 +0700

    Exercise 5 committed

---
+++ HelloWorld/src/com/ekwiwat/javaPart1Ex4_5/package-info.java
@@ -0,0 +1,19 @@
+package com.ekwiwat.javaPart1Ex4_5;
+
+class exercise4_5 {
+	public static void main(String[] args) {
+//		ข้อ 4.5 lab 9
+		for(int count = 0; count <= 20; count++) {
+			if(count == 11) {
+				continue;}
+			System.out.println("Lab 9 Count : " + count);
+		}
+		
+//		ข้อ 4.5 lab 10
+		for(int count = 0; count <= 10; count++) {
+			if(count == 2) {
+				continue;}
+			System.out.println("Lab 10 Count : " + count);
+		}
+	}
+	}
